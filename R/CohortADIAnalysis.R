d6a68771b9880a791b26638b215967461a7f8396
Zoey Jiang <36078824+xj2193@users.noreply.github.com>
2021-11-20 08:24:48 -0500

    Geocode adi (#17)
    * Add ADI Analysis into Package.
    
    * modified the extract geomtry data query
    
    * Add ADI Analysis flag in main.R

---
+++ R/CohortADIAnalysis.R
@@ -0,0 +1,76 @@
+# Copyright 2020 Observational Health Data Sciences and Informatics
+#
+# This file is part of cervello
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' @details
+#' Run the patient count and median ADI for each County
+#' 
+#' @param connection           An object of type \code{connection} that has an established database connection.
+#' @param cdmDatabaseSchema    Schema name where your patient-level data in OMOP CDM format resides.
+#'                             Note that for SQL Server, this should include both the database and
+#'                             schema name, for example 'cdm_data.dbo'.
+#' @param cohortDatabaseSchema Schema name where intermediate data can be stored. You will need to have
+#'                             write priviliges in this schema. Note that for SQL Server, this should
+#'                             include both the database and schema name, for example 'cdm_data.dbo'.
+#' @param cdmDatabaseSchema    Schema name where your patient-level data in OMOP CDM format resides.
+#'                             Note that for SQL Server, this should include both the database and
+#'                             schema name, for example 'cdm_data.dbo'.
+#' @param cohortTable          The name of the table that will be created in the work database schema.
+#'                             This table will hold the exposure and outcome cohorts used in this
+#'                             study.
+#' @param outputFolder         Name of local folder to place results; make sure to use forward slashes
+#'                             (/). Do not use a folder on a network drive since this greatly impacts
+#'                             performance.
+#' @param minCellCount         The minimum number of subjects contributing to a count before it can be included
+#'                             in packaged results.
+#'
+#' @export
+
+#Run the patient count and median ADI for each County. Any value less than minCellCount limit will be returned as a label. 
+CohortADIAnalysis <- function(connection, cohortDatabaseSchema, cdmDatabaseSchema, cohortTable, outputFolder, minCellCount=10){
+  ParallelLogger::logInfo(paste("Extracting Geometry and ADI Data"))
+  geom_table <- 'location_geocode_bg_adi'
+  sql <- SqlRender::loadRenderTranslateSql(sqlFilename = 'ExtractGeomBG.sql',
+                                       packageName = package,
+                                       dbms = attr(connection, "dbms"),
+                                       target_database_schema = cohortDatabaseSchema,
+                                       cdm_database_schema = cdmDatabaseSchema,
+                                       cohort_table = cohortTable,
+                                       adi_data = 'adi_data')
+  DatabaseConnector::executeSql(connection, sql)
+  adi_county_count_sql <- "SELECT cohort_definition_id, COUNTY_NAME, STATE_NAME, CASE WHEN COUNT(DISTINCT person_id) > @minCellCount THEN STR(COUNT(DISTINCT person_id)) ELSE CONCAT('<=', STR(@minCellCount)) END AS person_count 
+  FROM @cohort_database_schema.@table_name GROUP BY cohort_definition_id, STATE_NAME, COUNTY_NAME"
+  adi_county_count_sql <- SqlRender::render(adi_county_count_sql,
+                                      minCellCount = minCellCount,
+                           cohort_database_schema = cohortDatabaseSchema,
+                           table_name = geom_table)
+  adi_county_count_sql <- SqlRender::translate(adi_county_count_sql, targetDialect = attr(connection, "dbms"))
+  adi_county_count_result <- DatabaseConnector::querySql(connection, adi_county_count_sql)
+  adi_county_count_result$PERSON_COUNT <- as.character(adi_county_count_result$PERSON_COUNT)
+  
+  adi_county_median_sql <- "SELECT cohort_definition_id, COUNTY_NAME, STATE_NAME, person_id, ADI_STATERNK FROM @cohort_database_schema.@table_name"
+  adi_county_median_sql <- SqlRender::render(adi_county_median_sql,
+                                            cohort_database_schema = cohortDatabaseSchema,
+                                            table_name = geom_table)
+  adi_county_median_sql <- SqlRender::translate(adi_county_median_sql, targetDialect = attr(connection, "dbms"))
+  adi_county_median_result <- DatabaseConnector::querySql(connection, adi_county_median_sql)
+  adi_county_median_result <- adi_county_median_result[!is.na(as.numeric(as.character(adi_county_median_result$ADI_STATERNK))),]
+  
+  #adi_county_median_result <- adi_county_median_result[Reduce(`&`, lapply(adi_county_median_result$ADI_STATERNK, function(x) !is.na(as.numeric(as.character(x))))),]
+  adi_county_median_result <- adi_county_median_result %>% group_by(COHORT_DEFINITION_ID, STATE_NAME, COUNTY_NAME) %>% mutate(Median_ADI=median(ADI_STATERNK))
+  
+  write.csv(adi_county_count_result, file.path(outputFolder, "ADICountyCountResult.csv"))
+  write.csv(adi_county_median_result, file.path(outputFolder, "ADICountyMedianADIResult.csv"))
+}
--- R/Main.R
+++ R/Main.R
@@ -48,6 +48,7 @@
 #' @param synthesizePositiveControls  Should positive controls be synthesized?
 #' @param runAnalyses          Perform the cohort method analyses?
 #' @param runDiagnostics       Compute study diagnostics?
+#' @param runADIAnalysis       Is your location data geocoded available to run ADI?
 #' @param packageResults       Should results be packaged for later sharing?     
 #' @param maxCores             How many parallel cores should be used? If more cores are made available
 #'                             this can speed up the analyses.
@@ -87,6 +88,7 @@ execute <- function(connectionDetails,
                     runOhdsiCharacterization = TRUE,
                     runTreatmentAnalysis = TRUE,
                     runDiagnostics = TRUE,
+                    runADIAnalysis = TRUE,
                     packageResults = TRUE,
                     renderMarkdown = FALSE,
                     maxCores = 4,
@@ -98,7 +100,7 @@ execute <- function(connectionDetails,
   cohortTable <- "cancer_cohorts"
   package <- "CancerTreatmentCharacterization"
   ParallelLogger::logInfo("Initializing Study")
-  initializeStudy(outputFolder, connection, cohortDatabaseSchema, oracleTempSchema, package, reloadData)
+  initializeStudy(outputFolder, connection, cohortDatabaseSchema, oracleTempSchema, package, reloadData, runADIAnalysis)
 
   on.exit(ParallelLogger::unregisterLogger("DEFAULT_FILE_LOGGER", silent = TRUE))
   on.exit(ParallelLogger::unregisterLogger("DEFAULT_ERRORREPORT_LOGGER", silent = TRUE), add = TRUE)
@@ -244,6 +246,11 @@ execute <- function(connectionDetails,
                                         minCellCount)
       }
     }
+    
+    if (runADIAnalysis) {
+      ParallelLogger::logInfo("Run ADI Analysis to get County level patient counts and median ADI")
+      CohortADIAnalysis(connection, cohortDatabaseSchema, cdmDatabaseSchema, cohortTable, outputFolder, minCellCount = 10)
+    }
 
     if (packageResults) {
       ParallelLogger::logInfo("Packaging results")
@@ -261,19 +268,19 @@ execute <- function(connectionDetails,
 }
 
 # This function initializes all parameters, creates necessary folders, and loading data to the database for the study
-initializeStudy <- function(outputFolder, connection, cohortDatabaseSchema, oracleTempSchema, package, reloadData = TRUE) {
+initializeStudy <- function(outputFolder, connection, cohortDatabaseSchema, oracleTempSchema, package, reloadData = TRUE, runADIAnalysis = TRUE) {
   if (!file.exists(outputFolder))
     dir.create(outputFolder, recursive = TRUE)
   ParallelLogger::addDefaultFileLogger(file.path(outputFolder, "log.txt"))
   ParallelLogger::addDefaultErrorReportLogger(file.path(outputFolder, "errorReportR.txt"))
-
   if (reloadData) {
+    if(runADIAnalysis){ 
     #Load ADI data
-    # print(paste0("package: ", package))
-    # ParallelLogger::logInfo("Loading ADI Data")
-    # pathToCsv <- system.file("settings", "adi_state_data.csv", package = package)
-    # createAndLoadFileToTable(pathToCsv, sep = ",", connection, cohortDatabaseSchema, createTableFile = "CreateADITable.sql", tableName = "adi_data", targetDialect = attr(connection, "dbms"), oracleTempSchema, package)
-
+    print(paste0("package: ", package))
+    ParallelLogger::logInfo("Loading ADI Data")
+    pathToCsv <- system.file("settings", "US_2019_ADI_Census_Block_Group.csv", package = package)
+    createAndLoadFileToTable(pathToCsv, sep = ",", connection, cohortDatabaseSchema, createTableFile = "CreateADITable.sql", tableName = "adi_data", targetDialect = attr(connection, "dbms"), oracleTempSchema, package)
+    }
     #load CanMed
     ParallelLogger::logInfo("Loading CanMED Data")
     pathToCsv <- system.file("settings", "can_med.txt", package = package)
@@ -304,7 +311,12 @@ createAndLoadFileToTable <- function(pathToCsv, sep = ",", connection, cohortDat
 
 
   #Load data from csv file
-  data <- read.csv(file = pathToCsv, sep = sep)
+  if(tableName == "adi_data"){
+    data <- read.csv(file = pathToCsv, sep = sep, colClasses = c('character'))
+  }
+  else{
+    data <- read.csv(file = pathToCsv, sep = sep)
+  }
   #Construct the values to insert
   # paste0(apply(head(data), 1, function(x) paste0("('", paste0(x, collapse = "', '"), "')")), collapse = ", ")
   #batch load the rows
@@ -325,7 +337,12 @@ createAndLoadFileToTable <- function(pathToCsv, sep = ",", connection, cohortDat
         # v1 <- apply(i, 1, function(x) ifelse(is.na(strtoi(x)), paste0("'", x,"'"), paste0(x)))
         # v2 <- apply(v1, 2, function(x) paste(x, collapse = ", "))
         # values <- paste0("(", v2, ")", collapse=",")
-        values <- paste0("(", apply(apply(i, 1, function(x) ifelse(is.na(strtoi(x)), paste0("'", x,"'"), paste0(x))), 2, function(x) paste(x, collapse = ", ")), ")", collapse=",")
+        if (tableName == 'adi_data'){
+          values <- paste0("(", apply(apply(i, 1, function(x) paste0("'", x, "'")), 2, function(x) paste0(x, collapse = ", ")), ")", collapse=",")
+        }
+        else{
+          values <- paste0("(", apply(apply(i, 1, function(x) ifelse(is.na(strtoi(x)), paste0("'", x,"'"), paste0(x))), 2, function(x) paste(x, collapse = ", ")), ")", collapse=",")
+        }
       # }
 
     # }
